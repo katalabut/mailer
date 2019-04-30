3196e39c7e4166daa56fa2f0e4af56f4c0b61f98
gerenko <gerenko@obit.ru>
2019-04-30 18:00:44 +0300

    Added hadlers and env

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/spotbut/mailer
 
 go 1.12
+
+require (
+	github.com/kelseyhightower/envconfig v1.3.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+)
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type SmtpConf struct {
+	Host     string `required:"true"`
+	Port     int    `required:"true"`
+	Login    string `required:"true"`
+	Password string `required:"true"`
+}
+
 func main() {
 
+	os.Setenv("SMTP_HOST", "smtp.yandex.ru")
+	os.Setenv("SMTP_PORT", "465")
+
+	var sc SmtpConf
+	err := envconfig.Process("smtp", &sc)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	sendHandler := http.HandlerFunc(send)
+	http.Handle("/send", sendHandler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func send(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
 }
